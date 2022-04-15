4dde6f24a6e65abcbf73ea238adf90ca8454b751
Tidlsss <zero777960@gmail.com>
2022-04-16 01:46:11 +0900

    ..

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220416.0145';
+const cacheName = 'chirpy-20220416.0146';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
